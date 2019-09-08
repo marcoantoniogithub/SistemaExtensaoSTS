86d9f3b7c9faf8c5b95cc61227acbd5cef8500e2
Marco Antonio <marcoantoniozxcv@gmail.com>
2019-09-08 18:59:17 -0300

    criando projeto

---
+++ src/test/java/extensao/SistemaExtensaoStsApplicationTests.java
@@ -0,0 +1,13 @@
+package extensao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaExtensaoStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
